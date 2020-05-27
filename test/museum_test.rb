96f684f15cd189953d95bb3e53a252acdc12bff6
Gaby Mendez <gmendez90@gmail.com>
2020-05-27 12:16:05 -0400

    Add more tests for iteration 3

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -90,4 +90,36 @@ class MuseumTest < Minitest::Test
 
     assert_equal true, patrons_for_dead_sea.include?(@patron_2)
   end
+
+  def test_has_lottery_ticket_contestants
+    # for this test, Bob has no money
+    patron_1 = Patron.new("Bob", 0)
+
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    assert_equal 3, @dmns.exhibits.count
+
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3.add_interest("Dead Sea Scrolls")
+
+    @dmns.admit(patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+
+    assert_equal [patron_1, @patron_3], @dmns.ticket_lottery_contestants(@dead_sea_scrolls)
+
+    # use stub for next assertion
+    assert_equal "Bob", @dmns.draw_lottery_winner(@dead_sea_scrolls)
+
+    assert_nil @dmns.draw_lottery_winner(@gems_and_minerals)
+
+    assert_equal "Bob has won the IMAX exhibit lottery", @dmns.announce_lottery_winner(@imax)
+
+    assert_equal "No winners for this lottery", @dmns.announce_lottery_winner(@gems_and_minerals)
+  end
+
+
 end
