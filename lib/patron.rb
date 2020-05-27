7d93d170eaad4b85711d8684ff9f4a9ab372f06b
Gaby Mendez <gmendez90@gmail.com>
2020-05-27 11:16:34 -0400

    Pass interests test

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,7 +1,8 @@
 class Patron
-  attr_reader :name, :spending_money
+  attr_reader :name, :spending_money, :interests
   def initialize(name, money)
     @name = name
     @spending_money = money
+    @interests = []
   end
 end
